2c8ca6ead65beffc85d8b86cad8285386ff5a997
Dawid Borowski <109763767+Liandrel@users.noreply.github.com>
2023-04-18 07:32:47 +0200

    18.04.2023 daily
    Added solution to 18.04 daily leetcode challange

---
+++ 1768. Merge Strings Alternately/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using System;
+using System.Text;
+
+string[] firstWords = new string[] { "abc", "ab", "abcd" };
+string[] secondWords = new string[] { "pqr", "pqrs", "pq" };
+
+List<string> results = new();
+
+string MergeAlternately(string word1, string word2)
+{
+    int word1Length = word1.Length;
+    int word2Length = word2.Length;
+    int max = Math.Max(word1Length, word2Length);
+
+    StringBuilder result = new();
+
+    for (int i = 0; i < max; i++)
+    {
+        if (i < word1Length)
+            result.Append(word1[i]);
+        if (i < word2Length)
+            result.Append(word2[i]);
+    }
+
+    return result.ToString();
+}
+
+    for (int i = 0; i < firstWords.Length; i++)
+{
+    results.Add(MergeAlternately(firstWords[i], secondWords[i]));
+}
+
+
+Console.ReadLine();
--- LeetCodeDaily/Program.cs
+++ LeetCodeDaily/Program.cs
@@ -8,35 +8,35 @@ public class Solution
 {
     public int NumWays(string[] words, string target)
     {
-        int n = words[0].Length;
-        int m = target.Length;
+        int wordLength = words[0].Length;
+        int targetLength = target.Length;
         int mod = 1000000007;
-        int[] dp = new int[m + 1];
+        int[] dp = new int[targetLength + 1];
         dp[0] = 1;
 
-        int[][] count = new int[n][];
-        for (int i = 0; i < n; i++)
+        int[][] count = new int[wordLength][];
+        for (int i = 0; i < wordLength; i++)
         {
             count[i] = new int[26];
         }
 
         foreach (string word in words)
         {
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < wordLength; i++)
             {
                 count[i][word[i] - 'a']++;
             }
         }
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < wordLength; i++)
         {
-            for (int j = m - 1; j >= 0; j--)
+            for (int j = targetLength - 1; j >= 0; j--)
             {
                 dp[j + 1] += (int)((long)dp[j] * count[i][target[j] - 'a'] % mod);
                 dp[j + 1] %= mod;
             }
         }
 
-        return dp[m];
+        return dp[targetLength];
     }
 }
