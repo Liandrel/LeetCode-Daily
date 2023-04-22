eab2b36aa34478c9fb5c6f55104016e76fe5d068
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-04-22 19:37:44 +0200

    Longest common subsequence
    Added solution to leetcode challange longest common subsequence

---
+++ 1143. Longest Common Subsequence/Program.cs
@@ -0,0 +1,40 @@
+﻿
+List<string> text1s = new() { "abcde", "abc", "abc" };
+List<string> text2s = new() { "ace", "abc", "def" };
+
+
+for(int i = 0;  i < text1s.Count; i++)
+{
+    var result = LongestCommonSubsequence(text1s[i], text2s[i]);
+}
+
+
+
+ int LongestCommonSubsequence(string text1, string text2)
+{
+    int n = text1.Length;
+    int m = text2.Length;
+
+    int[,] dp = new int[n + 1, m + 1];
+
+    for (int j = 0; j <= m; j++)
+        dp[0, j] = 0;
+    for (int i = 0; i < n; i++)
+        dp[i, 0] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (text1[i - 1] == text2[j - 1])
+            {
+                dp[i, j] = 1 + dp[i - 1, j - 1];
+            }
+            else
+            {
+                dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+            }
+        }
+    }
+    return dp[n, m];
+}
--- 879. Profitable Schemes/Program.cs
+++ 879. Profitable Schemes/Program.cs
@@ -5,31 +5,31 @@ List<int[]> groups = new() { new int[] { 2, 2 }, new int[] { 2, 3, 5 } };
 List<int[]> profits = new() { new int[] { 2, 3 }, new int[] { 6, 7, 8 } };
 
 
-int ProfitableSchemes(int n, int minProfit, int[] group, int[] profit)
+int ProfitableSchemes(int membersAvaiable, int minProfit, int[] membersForCrime, int[] profitFromCrime)
 {
     int mod = 1000000007;
 
-    int[,] dp = new int[minProfit + 1, n + 1];
+    int[,] dp = new int[minProfit + 1, membersAvaiable + 1];
 
     dp[0, 0] = 1;
 
     int sum;
 
-    for (int i = 0; i < group.Length; i++)
+    for (int i = 0; i < membersForCrime.Length; i++)
     {
         for ( int p = minProfit; p >= 0; p--)
         {
-            for ( int j = n - group[i]; j >=0; j--)
+            for ( int j = membersAvaiable - membersForCrime[i]; j >=0; j--)
             {
-                sum = Math.Min(minProfit, p + profit[i]);
-                dp[sum, j + group[i]] = (dp[sum, j + group[i]] + dp[p, j]) % mod;
+                sum = Math.Min(minProfit, p + profitFromCrime[i]);
+                dp[sum, j + membersForCrime[i]] = (dp[sum, j + membersForCrime[i]] + dp[p, j]) % mod;
             }
         }
     }
 
     int result = 0;
 
-    for (int j = 0; j <= n; j++)
+    for (int j = 0; j <= membersAvaiable; j++)
     {
         result = (result + dp[minProfit, j]) % mod;
     }
