595f8d80953e16634a976faceb3375200d70997d
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-04-24 17:37:14 +0200

    509. Fibonacci Number
    Added solution to 509. Fibonacci Number

---
+++ 509. Fibonacci Number/Program.cs
@@ -0,0 +1,25 @@
+﻿List<int> numbers = new() { 2, 3, 4 };
+
+for(int i = 0; i <= numbers.Count; i++)
+{
+    var result = Fib(numbers[i]);
+}
+
+
+int Fib(int n)
+{
+    if (n == 0)
+        return 0;
+    if(n == 1 || n == 2)
+        return 1;
+
+    int prev2 = 0;
+    int prev = 1;
+    int cur = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        cur = prev2 + prev;
+        (prev2, prev) = (prev, cur);
+    }
+    return cur;
+}
