d016faac8d219c1b81d73428dc531221a26e4c78
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-04-24 18:02:31 +0200

    70. Climbing Stairs
    Added solution to 70. Climbing Stairs

---
+++ 70. Climbing Stairs/Program.cs
@@ -0,0 +1,24 @@
+﻿List<int> numbers = new() { 2, 3 };
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    var result = ClimbStairs(numbers[i]);
+}
+
+
+
+int ClimbStairs(int n)
+{
+    if (n <= 3)
+        return n;
+
+    int prev2 = 1;
+    int prev = 2;
+    int cur = 0;
+    for (int i = 3; i <= n; i++)
+    {
+        cur = prev + prev2;
+        (prev2, prev) = (prev, cur);
+    }
+    return cur;
+}
