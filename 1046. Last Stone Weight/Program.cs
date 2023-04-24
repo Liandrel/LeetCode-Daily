ab2cfeb6938224243a78751deeea657668287fd6
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-04-24 17:03:12 +0200

    24.04.2023 daily
    Added solution to 24.04 daily leetcode challange

---
+++ 1046. Last Stone Weight/Program.cs
@@ -0,0 +1,37 @@
+﻿List<int[]> stones = new() { new int[] { 2, 7, 4, 1, 8, 1 }, new int[] { 1 }, new int[] { 10, 10, 7, 2 } };
+
+
+
+for (int i = 0;i <= stones.Count;i++)
+{
+    var result = LastStoneWeight(stones[i]);
+}
+
+Console.ReadLine();
+
+int LastStoneWeight(int[] stones)
+{
+	int count = stones.Length;
+	while (count > 1)
+	{
+        Array.Sort(stones, (x, y) => y.CompareTo(x));
+
+        if (stones[0] == stones[1])
+		{
+			stones[0] = 0;
+			stones[1] = 0;
+			count -= 2;
+		}
+		else
+		{
+			stones[0] -= stones[1];
+			stones[1] = 0;
+			if (stones[0] != 0)
+				count -= 1;
+			else
+				count -= 2;
+
+        }
+    }
+	return stones.Max();
+}
