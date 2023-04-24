90b49f2244dc168ac2669ae140f171b62ab5dc26
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-04-24 18:59:32 +0200

    746. Min Cost Climbing Stairs
    Added solution to 746. Min Cost Climbing Stairs

---
+++ 746. Min Cost Climbing Stairs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+List<int[]> costs = new() { new int[] { 10, 15, 20 }, new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 } };
+
+for (int i = 0;i < costs.Count;i++)
+{
+    var result = MinCostClimbingStairs(costs[i]);
+}
+
+
+int MinCostClimbingStairs(int[] cost)
+{
+    var length = cost.Length;
+    var dp = new int[length + 1];
+
+    int prev2 = cost[0];
+    int prev = cost[1];
+    int cur;
+
+    for (int i = 2; i < length; i++)
+    {
+        cur = cost[i] + Math.Min(prev, prev2);
+        (prev2, prev) = (prev, cur);
+    }
+
+    return Math.Min(prev, prev2);
+}
