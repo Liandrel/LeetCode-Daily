77b57a83082b4c684b72564c68654c4bd5db6b2f
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-04-25 21:30:49 +0200

    62. Unique Paths
    Added solution to 62. Unique Paths

---
+++ 62. Unique Paths/Program.cs
@@ -0,0 +1,37 @@
+﻿List<int> m = new() { 3, 3 };
+List<int> n = new() { 7, 2 };
+
+for(int i = 0; i < m.Count; i++)
+{
+    var result = UniquePaths(m[i], n[i]);
+}
+
+
+int UniquePaths(int m, int n)
+{
+    int[] prev = new int[n + 1];
+    int[] cur = new int[n + 1];
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(i == 0 && j == 0)
+            {
+                cur[j] = 1;
+                continue;
+            }
+
+            int up = 0;
+            int left = 0;
+
+            if (i > 0)
+                up = prev[j];
+            if (j > 0)
+                left = cur[j - 1];
+            cur[j] = up + left;
+        }
+        prev = cur;
+        cur = new int[n + 1];
+    }
+    return prev[n-1];
+}
