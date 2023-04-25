46d9fac543dc377279b00cdb21a6e3f489b2c6a8
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-04-25 16:05:29 +0200

    25.04.2023 daily
    Added solution to 25.04 daily leetcode challange

---
+++ 2336. Smallest Number in Infinite Set/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+public class SmallestInfiniteSet
+{
+    public SmallestInfiniteSet()
+    {
+        _popedNumbers = new();
+        _cursor = 1;
+    }
+    private HashSet<int> _popedNumbers;
+    private int _cursor;
+
+    public int PopSmallest()
+    {
+        int output = _cursor;
+        _popedNumbers.Add(output);
+        for (int i = _cursor; i < int.MaxValue; i++)
+        {
+            if (_popedNumbers.Contains(i) == false)
+            {
+                _cursor = i;
+                break;
+            }
+        }
+        return output;
+    }
+
+    public void AddBack(int num)
+    {
+        _popedNumbers.Remove(num);
+        if (num < _cursor)
+            _cursor = num;
+    }
+}
---
+++ 338. Counting Bits/Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> numbers = new() { 2, 5 };
+
+
+for(int i = 0; i < numbers.Count; i++)
+{
+    var result = CountBits(numbers[i]);
+}
+
+int[] CountBits(int n)
+{
+    int[] dp = new int[n + 1];
+    for(int i = 0; i < dp.Length; i++)
+    {
+        dp[i] = i % 2 == 0 ? dp[i / 2] : dp[i - 1] + 1;
+    }
+    return dp;
+}
