deed320d0459c6cfe1ab0cf33da62fd090d987b4
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-04-26 15:53:03 +0200

    26.04.2023 daily
    Added solution to 26.04 daily leetcode challange

---
+++ 258. Add Digits/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    //Digital root formula
+    public int AddDigits(int num)
+    {
+        if (num == 0)
+        {
+            return 0;
+        }
+        return 1 + (num - 1) % 9;
+    }
+}
+
+//Iterative solution
+/*
+int AddDigits(int num)
+{
+    while (num >= 10)
+    {
+        int sum = 0;
+        while(num >=10)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        sum += num;
+        num = sum;
+    }
+    return num;
+}
+*/
